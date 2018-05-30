6f216adcc07c84d0a1c9af7262c77b24535908fc
rawaa <raw3a.aladdin@gmail.com>
2018-05-30 15:39:48 +0200

    ss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './core/signup/signup.component';
 
 /*should be Home Module*/
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  {path: '', component: HomeComponent},
+  {path: 'signup', component: SignupComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
-// import { PatientProfileComponent } from './profiles/patient-profile/patient-profile.component';
 import { ProfileModule } from './profiles/profile.module';
 import { TreatmentModule } from './treatments/treatment.module';
 import { AppointmentModule } from './appointments/appointment.module';
@@ -14,22 +12,15 @@ import { TreatmentComponent } from './treatments/treatment/treatment.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { PatientsModule } from './patient/patients.module';
 import { ReviewsModule } from './reviews/reviews.module';
-// import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { DrugsModule } from './drugs/drugs.module';
 import { PatientAppointmentQueueComponent } from './appointments/patient-appointment-queue/patient-appointment-queue';
-// import {LoginComponent} from './core/login/login.component';
- import {SignupComponent} from './core/signup/signup.component';
-
-
-//  import{appoinmentCardService} from '../shared/models/Services/appointmentcard-service'
+import {SignupComponent} from './core/signup/signup.component';
 import { HomeComponent } from './home/home.component';
 import { sharedModule } from './shared/shared.module';
 import { AppoinmentCardService } from './shared/models/Services/appointmentcard-service';
 import { FormsModule } from '@angular/forms';
 import { DemoModule } from './DEMO/module';
 import { DemoComponent } from './DEMO/component';
-// import { DemoModule } from './Demo/module';
-// import { DemoComponent } from './DEMO/component';
 
 @NgModule({
   declarations: [
@@ -48,20 +39,7 @@ import { DemoComponent } from './DEMO/component';
     ReviewsModule,
     DrugsModule,
     CoreModule ,
-    DemoModule,
     sharedModule ,
-
-  
-
-    RouterModule.forRoot([
-      {
-        path: '', component: HomeComponent}
-      ,
-      {
-        path:'calender', component:DemoComponent
-      },
-      {path:'signup',component:SignupComponent}
-    ]),
     AppointmentModule
   ],
   providers: [AppoinmentCardService],
