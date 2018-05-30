a78b514e52ad0dbd0d4072258ebd68939b46cbb6
Monica Monir <monica.monirgeorge@gmail.com>
2018-05-30 10:13:35 +0200

    No Edit in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { FacilityModule } from './facilities/facility.module';
 import { CoreModule } from './core/core.module';
 import { LoginComponent } from './core/login/login.component';
 import { SignupComponent } from './core/signup/signup.component';
+import { HeaderComponent } from './header-footer/header/header.component';
+import { FooterComponent} from './header-footer/footer/footer.component';
 
 
 
@@ -20,6 +22,8 @@ import { TreatmentComponent } from './treatments/treatment/treatment.component';
 import { AppointmentComponent } from './appointments/apponitment';
 import { AppointmentBookingFormComponent } from './appointments/appointment-booking-form/appointment-booking-form.component';
 import { MyhomeComponent } from './home/myhome/myhome.component';
+import { HeaderandFooterModule } from './header-footer/headerand-footer.module';
+
 
 @NgModule({
   declarations: [
@@ -38,7 +42,8 @@ import { MyhomeComponent } from './home/myhome/myhome.component';
     AppointmentModule,
     HomeModule,
     FacilityModule,
-    CoreModule
+    CoreModule,
+    HeaderandFooterModule
 
     ,
     RouterModule.forRoot([
@@ -47,7 +52,9 @@ import { MyhomeComponent } from './home/myhome/myhome.component';
       { path: 'appointment', component: AppointmentComponent },
       { path: 'login', component: LoginComponent },
       { path: 'signup', component: SignupComponent },
-      { path: 'home', component: MyhomeComponent }
+      { path: 'home', component: MyhomeComponent },
+      
+      
 
 
     ]),
