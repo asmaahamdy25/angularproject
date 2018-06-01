5b96245c2441e4aad094188ea617f3d9ca4fc6a0
rawaa <raw3a.aladdin@gmail.com>
2018-06-01 05:29:30 +0200

    asdas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './core/signup/signup.component';
+import { DemoComponent } from './DEMO/component';
 
 /*should be Home Module*/
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'signup', component: SignupComponent}
+  {path: 'signup', component: SignupComponent},
+  {path: 'demo', component: DemoComponent}
 ];
 
 @NgModule({
