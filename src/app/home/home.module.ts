3d1c0b22f10736c0c0c40352d6c75ece1f8b98cd
Galaxy <nourdash16@gmail.com>
2018-06-05 13:39:45 +0200

    slider

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { SliderComponent } from './slider/slider.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+/*import { CarouselModule } from 'ngx-bootstrap';*/
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,CarouselModule.forRoot()
   ],
   declarations: [HomeComponent,
     SliderComponent
