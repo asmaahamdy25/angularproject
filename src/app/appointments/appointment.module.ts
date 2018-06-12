6291596e0671140b1a66f8a9954f3d3505f35b65
Asmaa Elrosasy <asmaaelrosasy@gmail.com>
2018-06-12 15:20:25 +0200

    E

--- src/app/appointments/appointment.module.ts
+++ src/app/appointments/appointment.module.ts
@@ -4,30 +4,16 @@ import { AppointmentsRoutingModule } from './appointments-Routing.module';
 import { RouterModule } from '@angular/router';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { sharedModule } from '../shared/shared.module';
+import { SharedModule } from '../shared/shared.module';
 @NgModule({
-<<<<<<< HEAD
   declarations: [
 
   ],
   imports: [
     RouterModule,
     CommonModule,
-    AppointmentsRoutingModule 
-
+    AppointmentsRoutingModule
   ]
-=======
-  declarations: [],
-  imports: [
-    RouterModule,
-    CommonModule,
-    appointmentsRoutingModule,
-    FormsModule,
-    ReactiveFormsModule ,
-    sharedModule
-  ],
-  exports: [FormsModule]
->>>>>>> 179632b7d01c2cd5d19f782ea1ca6eb5a18945a6
 })
 export class AppointmentModule {
  }
